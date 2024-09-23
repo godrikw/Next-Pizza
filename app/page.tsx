ab0a2a8d76e2c6bf80ff501737fa573110b0e4bb
godrikw <you@example.com>
2024-09-23 22:35:57 +0300

    changes_0

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import {Button} from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button variant={"default"}>Корзинаasd</Button>
+      <Button variant={"default"}>Корзина</Button>
     </div>
   );
 }
