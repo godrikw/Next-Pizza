9be095fdf3018724e87bd2d932eab8497e207ce1
godrikw <you@example.com>
2024-10-04 00:03:54 +0300

    changes_03.10_1

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import { Container } from './container';
 import { Button } from '@/components/ui';
-import { ShoppingCart, User } from 'lucide-react';
+import { ArrowRight, ShoppingCart, User } from 'lucide-react';
 
 interface Props {
     className?: string;
@@ -47,12 +47,18 @@ export const Header: React.FC<Props> = ({ className }) => {
                         <Button className="group relative">
                             <b>520 ₽</b>
                             <span className="h-full w-[1px] bg-white/30 mx-3" />
-                            <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0"></div>
-                            <ShoppingCart
-                                className="h-4 w-4 relative"
-                                strokeWidth={2}
+                            <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                                <ShoppingCart
+                                    size={16}
+                                    className="relative"
+                                    strokeWidth={2}
+                                />
+                                <b>3</b>
+                            </div>
+                            <ArrowRight
+                                size={20}
+                                className=" absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"
                             />
-                            <b>3</b>
                         </Button>
                     </div>
                 </div>
